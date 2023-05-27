f0627ca884f34b9ea452a75e9c99974a8ab3eb17
Peter Verstraete <parus17@gmail.com>
2023-05-27 11:02:30 +0200

    Work in progress

---
+++ src/test/java/be/parus17/enersmart/NotificationServiceTests.java
@@ -0,0 +1,13 @@
+package be.parus17.enersmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
